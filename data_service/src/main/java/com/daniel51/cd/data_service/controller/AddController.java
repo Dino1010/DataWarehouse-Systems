8be515f441d18c73c44a68cfd4b9bde0d8058438
DanielGu <DanielGu@users.noreply.github.com>
2019-07-22 22:57:35 +0800

    v0.2 测试SpringBoot Evn

---
+++ data_service/src/main/java/com/daniel51/cd/data_service/controller/AddController.java
@@ -0,0 +1,16 @@
+package com.daniel51.cd.data_service.controller;/*
+ @author Daniel51
+ @DESCRIPTION add a,b
+ @create 2019/7/22
+*/
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AddController {
+    @RequestMapping("/add")
+    public int add(int a, int b){
+        return 2;
+    }
+}
---
+++ data_service/src/main/java/com/daniel51/cd/data_service/service/AddServer.java
@@ -0,0 +1,16 @@
+package com.daniel51.cd.data_service.service;/*
+ @author Daniel51
+ @DESCRIPTION 
+ @create 2019/7/22
+*/
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AddServer {
+
+    public int add(int a, int b){
+        return a+b;
+    }
+
+}
